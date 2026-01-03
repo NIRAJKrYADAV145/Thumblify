57aea8a2b062166d838cc80d6ded08aea14be2b5
NIRAJKrYADAV145 <niraj20802@gmail.com>
2026-01-03 23:31:42 +0530

    Design UI

---
+++ src/data/features.tsx
@@ -0,0 +1,20 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: " Our Ai analyzes video content to suggest the most clickable concepts.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Eye Catching Thumbnails",
+        description: "Generate vibrant, high-contrast thumbnails that stand out in the feed. ",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Get fully layered design you can tweak to perfection if needed.",
+    }
+    
+];
